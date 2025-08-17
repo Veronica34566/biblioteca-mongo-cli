4dd1231c750017b72deeb95247642fd63e308d24
Veronica34566 <151109064+Veronica34566@users.noreply.github.com>
2025-08-17 15:16:15 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (Contenido resumido: app.py completo del documento en canvas)
+# Ver documento proporcionado para el código íntegro
+print("Este es app.py - CLI Biblioteca con MongoDB y PyMongo")
